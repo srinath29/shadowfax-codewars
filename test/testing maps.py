8659e822a255d5ace468499359df836061d27512
srinath29 <srinath.prathi@gmail.com>
2016-06-11 07:52:02 +0100

    Added Data

--- test/testing maps.py
+++ test/testing maps.py
@@ -1,2 +1,13 @@
 import googlemaps
+import datetime
+import json
 
+gmaps = googlemaps.Client(key = "AIzaSyCTVcmF2m7PGO1UgEEv9Mfn-Vo0XbA5XYY")
+
+now = datetime.datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+
+print(json.loads( directions_result))
