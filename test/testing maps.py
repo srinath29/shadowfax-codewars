ada606d3d029789dfe527dc1d08ee78fff7d1c45
cmehta <chetanmehta0204@gmail.com>
2016-06-11 14:37:12 +0530

    added a test method.

--- test/testing maps.py
+++ test/testing maps.py
@@ -5,9 +5,16 @@ import json
 gmaps = googlemaps.Client(key = "AIzaSyCTVcmF2m7PGO1UgEEv9Mfn-Vo0XbA5XYY")
 
 now = datetime.datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
+# directions_result = gmaps.directions("Sydney Town Hall",
+#                                      "Parramatta, NSW",
+#                                      mode="transit",
+#                                      departure_time=now)
+# directions_result = gmaps.directions((12.985805, 77.737007),(12.992220,77.715910),
+#                                      mode="driving",
+#                                      departure_time=now)
+
+directions_result = gmaps.distance_matrix([(12.985805, 77.737007),(12.985805,77.737007)],[(12.992220,77.715910),(12.970853,77.715910)],
+                                     mode="driving",
                                      departure_time=now)
 
-print(json.loads( directions_result))
+print(directions_result)
